c807cdbc398a97c8ce601c15c560effaacdc661f
Pavlo-Petrashevskyi <pavlopetrashevskyi@gmail.com>
2024-11-11 15:30:45 +0200

    chore: save solution

---
+++ src/components/NoResults/NoResults.tsx
@@ -0,0 +1,11 @@
+type NoResultsProps = {
+  categoryName: string;
+};
+
+export const NoResults: React.FC<NoResultsProps> = ({ categoryName }) => {
+  return (
+    <div className="no-results-message">
+      {`We would like to apologise, we do not have available ${categoryName} right now :(`}
+    </div>
+  );
+};
